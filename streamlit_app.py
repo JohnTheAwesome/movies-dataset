2d4edefb962771e0711ec1c8f1ffb4187abb4496
JohnTheAwesome <67943455+JohnTheAwesome@users.noreply.github.com>
2025-01-29 09:47:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,3 @@
-import subprocess
-import sys
-
-# Function to install required packages
-def install(package):
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
-
-# List of required packages
-required_packages = ['yfinance', 'pandas', 'numpy', 'scikit-learn', 'streamlit', 'matplotlib']
-for package in required_packages:
-    try:
-        __import__(package)
-    except ImportError:
-        install(package)
-
-
 
 import yfinance as yf
 import pandas as pd
